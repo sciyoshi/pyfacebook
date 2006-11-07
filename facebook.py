7b9881cd0b031ed226d844dcdb87fe9788a3d702
Niran Babalola <iamniran@gmail.com>
2006-11-07 15:57:58 +0000

    Added next parameter for get_login_url and retabbed with spaces

--- facebook.py
+++ facebook.py
@@ -37,149 +37,151 @@ from xml.dom.minidom import parseString
 
 
 def _get_element_text(elem):
-	"""Get a node's text by joining all of the text child nodes."""
-	return ''.join(node.data for node in elem.childNodes if node.nodeType == node.TEXT_NODE)
+    """Get a node's text by joining all of the text child nodes."""
+    return ''.join(node.data for node in elem.childNodes if node.nodeType == node.TEXT_NODE)
 
 
 class FacebookError(Exception):
-	"""Exception class for errors received from Facebook."""
-	def __init__(self, info):
-		self.info = info
-	def __str__(self):
-		return ('Error ' + self.info['code'] + ': ' + self.info['msg'] + ' (' +
-				self.info['your_request']['method'] + ')')
+    """Exception class for errors received from Facebook."""
+    def __init__(self, info):
+        self.info = info
+    def __str__(self):
+        return ('Error ' + self.info['code'] + ': ' + self.info['msg'] + ' (' +
+                self.info['your_request']['method'] + ')')
 
 
 class Facebook(object):
-	def __init__(self, api_key, secret_key, auth_token=None):
-		self.api_key = api_key
-		self.secret_key = secret_key
-		self.secret = None
-		self.auth_token = auth_token
+    def __init__(self, api_key, secret_key, auth_token=None):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.secret = None
+        self.auth_token = auth_token
 
-	def auth_createToken(self):
-		result = self._call_method('facebook.auth.createToken', {})
-		self.auth_token = result['token']
-		return self.auth_token
+    def auth_createToken(self):
+        result = self._call_method('facebook.auth.createToken', {})
+        self.auth_token = result['token']
+        return self.auth_token
 
-	def auth_getSession(self):
-		result = self._call_method('facebook.auth.getSession', {'auth_token': self.auth_token})
-		self.session_key = result['session_key']
-		self.uid = result['uid']
-		# don't complain if there isn't a 'secret'. web apps don't have one
-		self.secret = result.get('secret')
-		return result
+    def auth_getSession(self):
+        result = self._call_method('facebook.auth.getSession', {'auth_token': self.auth_token})
+        self.session_key = result['session_key']
+        self.uid = result['uid']
+        # don't complain if there isn't a 'secret'. web apps don't have one
+        self.secret = result.get('secret')
+        return result
 
-	def wall_getCount(self, user=None):
-		if not user:
-			user = self.uid
-		return self._call_method('facebook.wall.getCount', {'id': user})
+    def wall_getCount(self, user=None):
+        if not user:
+            user = self.uid
+        return self._call_method('facebook.wall.getCount', {'id': user})
 
-	def users_getInfo(self, users=None, fields=['name']):
-		if not users:
-			users = [self.uid]
-		return self._call_method('facebook.users.getInfo', {'users': ','.join(users), 'fields': ','.join(fields)})
+    def users_getInfo(self, users=None, fields=['name']):
+        if not users:
+            users = [self.uid]
+        return self._call_method('facebook.users.getInfo', {'users': ','.join(users), 'fields': ','.join(fields)})
 
-	def events_getInWindow(self, start, end):
-		return self._call_method('facebook.events.getInWindow', {'start_time': str(start), 'end_time': str(end)})
+    def events_getInWindow(self, start, end):
+        return self._call_method('facebook.events.getInWindow', {'start_time': str(start), 'end_time': str(end)})
 
-	def pokes_getCount(self):
-		return self._call_method('facebook.pokes.getCount', {})
+    def pokes_getCount(self):
+        return self._call_method('facebook.pokes.getCount', {})
 
-	def photos_getAlbums(self, user=None):
-		if not user:
-			user = self.uid
-		return self._call_method('facebook.photos.getAlbums', {'id': user})
+    def photos_getAlbums(self, user=None):
+        if not user:
+            user = self.uid
+        return self._call_method('facebook.photos.getAlbums', {'id': user})
 
-	def photos_getCommentCount(self):
-		return self._call_method('facebook.photos.getCommentCount')
+    def photos_getCommentCount(self):
+        return self._call_method('facebook.photos.getCommentCount')
 
-	def photos_getFromAlbum(self, album):
-		return self._call_method('facebook.photos.getFromAlbum', {'aid': album})
+    def photos_getFromAlbum(self, album):
+        return self._call_method('facebook.photos.getFromAlbum', {'aid': album})
 
-	def messages_getCount(self):
-		return self._call_method('facebook.messages.getCount', {})
+    def messages_getCount(self):
+        return self._call_method('facebook.messages.getCount', {})
 
-	def friends_get(self):
-		return self._call_method('facebook.friends.get', {})
+    def friends_get(self):
+        return self._call_method('facebook.friends.get', {})
 
-	def friends_areFriends(self, id1, id2):
-		return self._call_method('facebook.friends.areFriends', {'id1': ','.join(id1), 'id2': ','.join(id2)})
+    def friends_areFriends(self, id1, id2):
+        return self._call_method('facebook.friends.areFriends', {'id1': ','.join(id1), 'id2': ','.join(id2)})
 
-	def friends_getTyped(self, type):
-		return self._call_method('facebook.friends.getTyped', {'link_type': type})
+    def friends_getTyped(self, type):
+        return self._call_method('facebook.friends.getTyped', {'link_type': type})
 
-	def photos_getOfUser(self, user=None, max=20):
-		if not user:
-			user = self.uid
-		return self._call_method('facebook.photos.getOfUser', {'id': user, 'max': str(max)})
+    def photos_getOfUser(self, user=None, max=20):
+        if not user:
+            user = self.uid
+        return self._call_method('facebook.photos.getOfUser', {'id': user, 'max': str(max)})
 
-	def get_login_url(self):
-		url = 'http://api.facebook.com/login.php?api_key=' + self.api_key
-		if self.auth_token is not None:
-			url += '&auth_token=' + self.auth_token
-		return url
-	
-	def login(self):
-		webbrowser.open(self.get_login_url())
+    def get_login_url(self, next=None):
+        url = 'http://api.facebook.com/login.php?api_key=' + self.api_key
+        if next is not None:
+            url += '&next=' + next
+        if self.auth_token is not None:
+            url += '&auth_token=' + self.auth_token
+        return url
+    
+    def login(self):
+        webbrowser.open(self.get_login_url())
 
 
 
-	def _parse_response_item(self, node):
-		if len(filter(lambda x: x.nodeType == x.ELEMENT_NODE and x.nodeName.endswith('_elt'), node.childNodes)) > 0:
-			return self._parse_response_list(node)
-		elif len(filter(lambda x: x.nodeType == x.ELEMENT_NODE, node.childNodes)) > 0:
-			return self._parse_response_dict(node)
-		else:
-			return _get_element_text(node)
+    def _parse_response_item(self, node):
+        if len(filter(lambda x: x.nodeType == x.ELEMENT_NODE and x.nodeName.endswith('_elt'), node.childNodes)) > 0:
+            return self._parse_response_list(node)
+        elif len(filter(lambda x: x.nodeType == x.ELEMENT_NODE, node.childNodes)) > 0:
+            return self._parse_response_dict(node)
+        else:
+            return _get_element_text(node)
 
-	def _parse_response_dict(self, node):
-		result = {}
-		for item in filter(lambda x: x.nodeType == x.ELEMENT_NODE, node.childNodes):
-			result[item.nodeName] = self._parse_response_item(item)
-		if node.nodeType == node.ELEMENT_NODE and node.hasAttributes():
-			if node.hasAttribute('id'):
-				result['id'] = node.getAttribute('id')
-		return result
+    def _parse_response_dict(self, node):
+        result = {}
+        for item in filter(lambda x: x.nodeType == x.ELEMENT_NODE, node.childNodes):
+            result[item.nodeName] = self._parse_response_item(item)
+        if node.nodeType == node.ELEMENT_NODE and node.hasAttributes():
+            if node.hasAttribute('id'):
+                result['id'] = node.getAttribute('id')
+        return result
 
-	def _parse_response_list(self, node):
-		result = []
-		for item in filter(lambda x: x.nodeType == x.ELEMENT_NODE, node.childNodes):
-			result.append(self._parse_response_item(item))
-		return result
-		
+    def _parse_response_list(self, node):
+        result = []
+        for item in filter(lambda x: x.nodeType == x.ELEMENT_NODE, node.childNodes):
+            result.append(self._parse_response_item(item))
+        return result
+        
 
-	def _check_error(self, result):
-		if type(result) is dict and result.has_key('fb_error'):
-			raise FacebookError, result['fb_error']
-		return True
+    def _check_error(self, result):
+        if type(result) is dict and result.has_key('fb_error'):
+            raise FacebookError, result['fb_error']
+        return True
 
-	def _arg_hash(self, args):
-		hash = md5.new()
-		hash.update(''.join([key + '=' + args[key] for key in sorted(args.keys())]))
-		if self.secret:
-			hash.update(self.secret)
-		else:
-			hash.update(self.secret_key)
-		return hash.hexdigest()
+    def _arg_hash(self, args):
+        hash = md5.new()
+        hash.update(''.join([key + '=' + args[key] for key in sorted(args.keys())]))
+        if self.secret:
+            hash.update(self.secret)
+        else:
+            hash.update(self.secret_key)
+        return hash.hexdigest()
 
-	def _call_method(self, method, args={}):
-		args['api_key'] = self.api_key
-		args['method'] = method
+    def _call_method(self, method, args={}):
+        args['api_key'] = self.api_key
+        args['method'] = method
 
-		if method not in ['facebook.auth.createToken', 'facebook.auth.getSession']:
-			args['session_key'] = self.session_key
-			args['call_id'] = str(int(time() * 1000))
+        if method not in ['facebook.auth.createToken', 'facebook.auth.getSession']:
+            args['session_key'] = self.session_key
+            args['call_id'] = str(int(time() * 1000))
 
-		args['sig'] = self._arg_hash(args)
+        args['sig'] = self._arg_hash(args)
 
-		if method == 'facebook.auth.getSession':
-			xml = urllib2.urlopen('https://api.facebook.com/restserver.php', urllib.urlencode(args)).read()
-		else:
-			xml = urllib2.urlopen('http://api.facebook.com/restserver.php', urllib.urlencode(args)).read()
+        if method == 'facebook.auth.getSession':
+            xml = urllib2.urlopen('https://api.facebook.com/restserver.php', urllib.urlencode(args)).read()
+        else:
+            xml = urllib2.urlopen('http://api.facebook.com/restserver.php', urllib.urlencode(args)).read()
 
-		dom = parseString(xml)
-		result = self._parse_response_item(dom)['result']
-		dom.unlink()
-		self._check_error(result)
-		return result
+        dom = parseString(xml)
+        result = self._parse_response_item(dom)['result']
+        dom.unlink()
+        self._check_error(result)
+        return result
