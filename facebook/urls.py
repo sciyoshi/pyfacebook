406a51404869311741ee6309912887934c1b989b
Samuel Cormier-Iijima <sciyoshi@gmail.com>
2007-05-27 15:33:41 +0000

    project decoupling hack

--- facebook/urls.py
+++ facebook/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls.defaults import *
 
+# Hack to get the project name
+project = __name__.split('.')[0]
+
 urlpatterns = patterns('',
     # This is the canvas callback, i.e. what will be seen
     # when you visit http://apps.facebook.com/<appname>.
-    (r'^canvas/', 'facebook.views.canvas'),
+    (r'^canvas/', project + '.facebook.views.canvas'),
 
     # Extra callbacks can be set in the Facebook app settings
     # page. For example, post_add will be called when a user
     # has added the application.
-    (r'^post_add/', 'facebook.views.post_add'),
+    (r'^post_add/', project + '.facebook.views.post_add'),
 )
