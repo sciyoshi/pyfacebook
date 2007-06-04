e80e4a2ec005c9fb14c4c4e1ffa16a5e6d47d1c1
Samuel Cormier-Iijima <sciyoshi@gmail.com>
2007-06-04 17:41:36 +0000

    added distutils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyfacebook',
+      version='0.1',
+      description='Python Client Library for the Facebook API',
+      author='Samuel Cormier-Iijima',
+      author_email='sciyoshi@gmail.com',
+      url='http://code.google.com/p/pyfacebook',
+      py_modules=['facebook'])
