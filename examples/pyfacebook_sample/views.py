96a854ef2e9f2d6e7794511777850df5d8ec0635
Samuel Cormier-Iijima <sciyoshi@gmail.com>
2007-08-08 06:11:15 +0000

    updated the example app

--- examples/pyfacebook_sample/views.py
+++ examples/pyfacebook_sample/views.py
@@ -1,24 +1,17 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 
-import urllib
-
-import facebook
-
-api_key = ''
-secret_key = ''
+import facebook.djangofb as facebook
 
 @facebook.require_login()
 def canvas(request):
-    if not request.facebook.in_canvas:
-        return request.facebook.redirect('http://apps.facebook.com/pyfacebook/')
-
+    # If you're using FBML, it'd be better to use <fb:name> than to do this - this is just as an example
     values = request.facebook.users.getInfo([request.facebook.uid], ['first_name', 'is_app_user', 'has_added_app'])[0]
 
     name, is_app_user, has_added_app = values['first_name'], values['is_app_user'], values['has_added_app']
 
     if has_added_app == '0':
-        return request.facebook.redirect(request.facebook.get_url('add', api_key=api_key))
+        return request.facebook.redirect(request.facebook.get_add_url())
 
     return render_to_response('facebook/canvas.fbml', {'name': name})
 
