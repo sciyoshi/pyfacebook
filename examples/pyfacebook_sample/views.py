29c9c507b09f9fceaf1716ca84155a6bab0cc9fe
Samuel Cormier-Iijima <sciyoshi@gmail.com>
2008-01-23 15:47:46 +0000

    Fix a bug in the sample app, thanks to Elena Tatarchenko <e.tatarchenko@gmail.com>

--- examples/pyfacebook_sample/views.py
+++ examples/pyfacebook_sample/views.py
@@ -23,7 +23,7 @@ def post(request):
 
 @facebook.require_login()
 def post_add(request):
-    request.facebook.profile.setFBML('Congratulations on adding PyFaceBook. Please click on the PyFaceBook link on the left side to change this text.', request.facebook.uid)
+    request.facebook.profile.setFBML(request.facebook.uid, 'Congratulations on adding PyFaceBook. Please click on the PyFaceBook link on the left side to change this text.')
 
     return request.facebook.redirect('http://apps.facebook.com/pyfacebook/')
 
