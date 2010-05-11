abb0e03c30c148fcbdbc5adfd0b61bb84248cee3
cheesesandwich <cheesesandwich@thinked-game.(none)>
2010-05-11 19:25:32 +0000

    Implementing dashboard.addNews

--- facebook/__init__.py
+++ facebook/__init__.py
@@ -239,6 +239,14 @@ METHODS = {
         'getAppUsers': [],
     },
 
+    'dashboard': {
+        'addNews': [
+            ('uid', int, []),
+            ('news', json, []),
+            ('image', str, ['optional']),
+        ],
+    },
+
     # notifications methods
     'notifications': {
         'get': [],
@@ -524,6 +532,20 @@ METHODS = {
         ],
 
         'getUnconnectedFriendsCount': [
+        ],
+    },
+
+    'links': {
+        'post': [
+            ('url', str, []),
+            ('comment', str, []),
+            ('uid', int, []),
+            ('image', str, ['optional']),
+            ('callback', str, ['optional']),
+        ],
+        'preview': [
+            ('url', str, []),
+            ('callback', str, ['optional']),
         ],
     },
 
@@ -1010,6 +1032,7 @@ class Facebook(object):
     def _check_error(self, response):
         """Checks if the given Facebook response is an error, and then raises the appropriate exception."""
         if type(response) is dict and response.has_key('error_code'):
+            raise Exception(response) # XXX Marinho
             raise FacebookError(response['error_code'], response['error_msg'], response['request_args'])
 
 
