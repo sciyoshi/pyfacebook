10463e32d1c0b9f2fd074aa3dbc7d5def09cb32b
woodcoder <andy@getdesignthing.com>
2010-05-26 09:41:52 +0100

    Changed version - using 1.0 as 'transitional'.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyfacebook',
-      version='2.0',
+      version='1.0a1',
       description='Python Client Library for the Facebook API',
       author='Samuel Cormier-Iijima',
       author_email='sciyoshi@gmail.com',
