4a5c019db9edfd0cb04420199e1a0b4d7ce0e67c
The Web Server <support@thinkeducationusa.com>
2010-06-30 20:22:57 +0000

    Fixing admin.setRestrictionInfo

--- facebook/__init__.py
+++ facebook/__init__.py
@@ -129,7 +129,7 @@ METHODS = {
         ],
         'getRestrictionInfo': [],
         'setRestrictionInfo': [
-            ('format', str, []),
+            ('format', str, ['optional']),
             ('callback', str, ['optional']),
             ('restriction_str', json, ['optional']),
         ],
