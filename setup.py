e855da23598fb52383bb25a69993020cb2fd96d4
unknown <Gordon@.(none)>
2011-03-02 12:10:50 -0500

    bump version for easy_install

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyfacebook',
-      version='1.0a2',
+      version='1.0a2.1',
       description='Python Client Library for the Facebook API',
       author='Samuel Cormier-Iijima',
       author_email='sciyoshi@gmail.com',
